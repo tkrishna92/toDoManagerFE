3ae2f7d62ee7c44b565d71566d0195c60559709b
tkrishna92 <tkrishnat92@gmail.com>
2019-12-11 02:24:14 +0530

    toDoManagerFE1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,52 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+//importing start
+
+// angular
+ import {RouterModule , Routes} from '@angular/router';
+ import {HttpClientModule} from '@angular/common/http';
+ import {FormsModule} from '@angular/forms';
+ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+ import {ToastrModule} from 'ngx-toastr';
+ import {CookieService} from 'ngx-cookie-service';
+
+
+ // components, modules and services
+  
+import { UserModule } from './user/user.module';
+import { TodoModule } from './todo/todo.module';
+import { SharedModule } from './shared/shared.module';
+import { LoginComponent } from './user/login/login.component';
+
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    TodoModule,
+    UserModule,
+    SharedModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+    RouterModule.forRoot([
+      {path : 'login', component : LoginComponent, pathMatch : 'full'},
+      {path : '', redirectTo : 'login', pathMatch : 'full'},
+      {path : '*', component : LoginComponent},
+      {path : '**', component : LoginComponent}
+    ])
+
+    
+
+
   ],
-  providers: [],
+  providers: [HttpClientModule, CookieService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/todo/todo.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TodoManagerComponent } from './todo-manager/todo-manager.component';
+
+
+
+@NgModule({
+  declarations: [TodoManagerComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class TodoModule { }
